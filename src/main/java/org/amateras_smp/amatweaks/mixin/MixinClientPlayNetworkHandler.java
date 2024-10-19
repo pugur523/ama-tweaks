b25c0a4b6b55fc9059ac21e9ebde9c38d6c5c8c6
pugur <ryota52399@gmail.com>
2024-10-19 09:36:28 +0900

    add screen mixin

--- src/main/java/org/amateras_smp/amatweaks/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/org/amateras_smp/amatweaks/mixin/MixinClientPlayNetworkHandler.java
@@ -1,5 +1,8 @@
 package org.amateras_smp.amatweaks.mixin;
 
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import me.fallenbreath.tweakermore.util.ModIds;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
@@ -9,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Restriction(require = @Condition(ModIds.itemscroller))
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class MixinClientPlayNetworkHandler {
     @Inject(
---
+++ src/main/java/org/amateras_smp/amatweaks/mixin/MixinScreen.java
@@ -0,0 +1,29 @@
+package org.amateras_smp.amatweaks.mixin;
+
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import me.fallenbreath.tweakermore.util.ModIds;
+import net.minecraft.client.gui.screen.Screen;
+import org.amateras_smp.amatweaks.util.container.AutoProcessableScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+
+@Restriction(require = @Condition(ModIds.itemscroller))
+@Mixin(Screen.class)
+public abstract class MixinScreen implements AutoProcessableScreen
+{
+    @Unique
+    private boolean shouldProcessFlag = false;
+
+    @Override
+    public void setShouldProcess$AMT(boolean value)
+    {
+        this.shouldProcessFlag = value;
+    }
+
+    @Override
+    public boolean shouldProcess$AMT()
+    {
+        return this.shouldProcessFlag;
+    }
+}
