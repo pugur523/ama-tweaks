217dee2b72e23d2a76405d264c7d20226c8b6d23
pugur <ryota52399@gmail.com>
2024-10-26 12:39:35 +0900

    small fix

--- src/main/java/org/amateras_smp/amatweaks/mixins/features/persistentGammaOverride/MixinClientPlayNetworkHandler.java
+++ src/main/java/org/amateras_smp/amatweaks/mixins/features/persistentGammaOverride/MixinClientPlayNetworkHandler.java
@@ -1,12 +1,16 @@
 package org.amateras_smp.amatweaks.mixins.features.persistentGammaOverride;
 
+import me.fallenbreath.conditionalmixin.api.annotation.Condition;
+import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import org.amateras_smp.amatweaks.Reference;
 import org.amateras_smp.amatweaks.config.FeatureToggle;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Restriction(require = @Condition(Reference.ModIds.tweakeroo))
 @Mixin(ClientPlayNetworkHandler.class)
 public class MixinClientPlayNetworkHandler {
     @Inject(method = "onGameJoin", at = @At("TAIL"))
