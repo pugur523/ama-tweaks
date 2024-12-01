e88a5537b29aabc3b2d7e492f94fb32e4acb1dd8
pugur <ryota52399@gmail.com>
2024-12-01 14:16:07 +0900

    fix compatibility issue

--- src/main/java/org/amateras_smp/amatweaks/InitHandler.java
+++ src/main/java/org/amateras_smp/amatweaks/InitHandler.java
@@ -3,13 +3,18 @@ package org.amateras_smp.amatweaks;
 import fi.dy.masa.malilib.config.ConfigManager;
 import fi.dy.masa.malilib.event.InputEventHandler;
 import fi.dy.masa.malilib.interfaces.IInitializationHandler;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import org.amateras_smp.amatweaks.config.Callbacks;
 import org.amateras_smp.amatweaks.config.Configs;
 import org.amateras_smp.amatweaks.event.InputHandler;
 import org.amateras_smp.amatweaks.impl.command.LookBackInteractionCommand;
 
+//#if MC >= 11900
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+//#else
+//$$ import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+//$$ import static net.fabricmc.fabric.api.client.command.v1.ClientCommandManager.literal;
+//#endif
 
 public class InitHandler implements IInitializationHandler
 {
@@ -26,7 +31,12 @@ public class InitHandler implements IInitializationHandler
     }
 
     public static void registerCommands() {
+        //#if MC >= 11900
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(literal("lookBack")
                 .executes(LookBackInteractionCommand.command)));
+        //#else
+        //$$ ClientCommandManager.DISPATCHER.register(literal("lookBack")
+        //$$    .executes(LookBackInteractionCommand.command));
+        //#endif
     }
 }
